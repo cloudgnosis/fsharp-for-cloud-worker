4c38b9953fc871ac19c125c230d3df7de452c9e2
Erik Lundevall Zara <45368867+eriklz@users.noreply.github.com>
2020-08-24 00:28:02 +0200

    Added processing of instance data

--- scripts/ShowServers.fsx
+++ scripts/ShowServers.fsx
@@ -4,9 +4,37 @@
 #r "nuget: AWSSDK.Core"
 #r "nuget: AWSSDK.EC2"
 
+open System
 open System.Environment
 open Amazon.EC2
 open Amazon.EC2.Model
+
+
+type InstanceInfo =
+    { InstanceId:       string
+      Name:             string
+      InstanceType:     string
+      PrivateIpAddress: string
+      LaunchTime:       DateTime
+      State:            string }
+
+      
+let rec getTagValue key (tags: Tag list) =
+    match tags with
+    | [] -> ""
+    | head :: tail -> if head.Key = key then head.Value  else getTagValue key tail
+
+
+let getInstanceInfo (instance: Instance) =
+    let tags = List.ofSeq instance.Tags
+    {
+        InstanceId = instance.InstanceId
+        Name = getTagValue "Name" tags
+        InstanceType = instance.InstanceType.Value
+        PrivateIpAddress = instance.PrivateIpAddress
+        LaunchTime = instance.LaunchTime
+        State = instance.State.Name.Value
+    }
 
 
 let getInstances (reservations: Reservation list) =
@@ -18,7 +46,7 @@ let describeInstances (client: AmazonEC2Client) =
         let! response = client.DescribeInstancesAsync() |> Async.AwaitTask
         return response
     }
-
+    
 
 let showServers (args: string[]) =
     if args.Length > 0 then
@@ -38,4 +66,9 @@ showServers (fsi.CommandLineArgs |> Array.skip 1)
   GetEnvironmentVariable("AWS_PROFILE")
   let mylist = List.ofSeq response.Reservations
   let response = showServers [| "erik" |]
+  let iis = showServers [| "erik" |] |> List.map getInstanceInfo
+  getInstanceInfo response.[0]
+  let instance = response.[0]
+  let tags = List.ofSeq instance.Tags
+  getTagValue "Name" tags
 *)
