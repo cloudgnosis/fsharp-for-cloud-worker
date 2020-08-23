c0029a773ef1c5cf5260bba54ecfa7d5b4c57ec7
Erik Lundevall Zara <45368867+eriklz@users.noreply.github.com>
2020-08-24 00:26:13 +0200

    Added handling for list of Instances.

--- scripts/ShowServers.fsx
+++ scripts/ShowServers.fsx
@@ -8,23 +8,34 @@ open System.Environment
 open Amazon.EC2
 open Amazon.EC2.Model
 
+
+let getInstances (reservations: Reservation list) =
+    reservations |> List.collect (fun x -> List.ofSeq x.Instances)
+
+
 let describeInstances (client: AmazonEC2Client) =
     async {
         let! response = client.DescribeInstancesAsync() |> Async.AwaitTask
         return response
     }
-    
+
+
 let showServers (args: string[]) =
     if args.Length > 0 then
         do SetEnvironmentVariable ("AWS_PROFILE", args.[0])
     let client = new AmazonEC2Client()
     let response = (describeInstances client) |> Async.RunSynchronously
-    response
+    if response.Reservations.Count = 0 then
+        List.empty<Instance>
+    else
+        (List.ofSeq response.Reservations) |> getInstances
+
     
 showServers (fsi.CommandLineArgs |> Array.skip 1)
 
 (*
   showServers [| "erik" |]
   GetEnvironmentVariable("AWS_PROFILE")
+  let mylist = List.ofSeq response.Reservations
   let response = showServers [| "erik" |]
 *)
