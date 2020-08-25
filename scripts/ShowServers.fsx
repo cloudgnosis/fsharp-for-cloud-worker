485c583b031a1d44212a0eab67c0f98a2bbd6479
Erik Lundevall Zara <45368867+eriklz@users.noreply.github.com>
2020-08-25 10:40:42 +0200

    Changed type IpAddrress to IpAddressOrDns

--- scripts/ShowServers.fsx
+++ scripts/ShowServers.fsx
@@ -6,20 +6,25 @@
 
 open System
 open System.Environment
-open System.Text.RegularExpressions
 open Amazon.EC2
 open Amazon.EC2.Model
 
-type IpAddress = IpAddress of string
-let toString (IpAddress a)  = a
+type IpAddressOrDns = 
+    | IpAddress of string
+    | DnsName of string
+
+let toString v  = 
+    match v with
+    | IpAddress s -> s
+    | DnsName s -> s
 
 type InstanceInfo =
-    { InstanceId:       string
-      Name:             string
-      InstanceType:     string
-      PrivateIpAddress: IpAddress
-      LaunchTime:       DateTime
-      State:            string }
+    { InstanceId:         string
+      Name:               string
+      InstanceType:       string
+      PrivateHostAddress: IpAddressOrDns
+      LaunchTime:         DateTime
+      State:              string }
 
 
 let rec getTagValue key (tags: Tag list) =
@@ -34,7 +39,7 @@ let getInstanceInfo (instance: Instance) =
         InstanceId = instance.InstanceId
         Name = getTagValue "Name" tags
         InstanceType = instance.InstanceType.Value
-        PrivateIpAddress = IpAddress instance.PrivateIpAddress
+        PrivateHostAddress = IpAddress instance.PrivateIpAddress
         LaunchTime = instance.LaunchTime
         State = instance.State.Name.Value
     }
@@ -48,11 +53,11 @@ let printInstanceInfo (instanceInfos: InstanceInfo list) : unit =
     if instanceInfos.IsEmpty then
         printfn "No instance info available!"
     else
-        printfn  "%-20s %-20s %-16s %-16s %-20s %-10s"
-            "InstanceId" "Name" "InstanceType" "Private IP" "LaunchTime" "State"
+        printfn  "%-20s %-20s %-16s %-21s %-20s %-10s"
+            "InstanceId" "Name" "InstanceType" "Private Host Address" "LaunchTime" "State"
         for ii in instanceInfos do
-            printfn "%-20s %-20s %-16s %-16s %-20s %-10s"
-                ii.InstanceId ii.Name ii.InstanceType (toString ii.PrivateIpAddress) (yyyymmddhhmmss ii.LaunchTime) ii.State
+            printfn "%-20s %-20s %-16s %-21s %-20s %-10s"
+                ii.InstanceId ii.Name ii.InstanceType (toString ii.PrivateHostAddress) (yyyymmddhhmmss ii.LaunchTime) ii.State
 
 
 let getInstances (reservations: Reservation list) =
