1fe2a2ec3cda55591b023689a38765f1b0ce86d8
Erik Lundevall Zara <45368867+eriklz@users.noreply.github.com>
2020-08-31 17:19:36 +0200

    Added HelloLambda with empty function template init.

---
+++ HelloLambda/test/HelloLambda.Tests/FunctionTest.fs
@@ -0,0 +1,21 @@
+namespace HelloLambda.Tests
+
+
+open Xunit
+open Amazon.Lambda.TestUtilities
+
+open HelloLambda
+
+
+module FunctionTest =
+    [<Fact>]
+    let ``Invoke ToUpper Lambda Function``() =
+        // Invoke the lambda function and confirm the string was upper cased.
+        let lambdaFunction = Function()
+        let context = TestLambdaContext()
+        let upperCase = lambdaFunction.FunctionHandler "hello world" context
+
+        Assert.Equal("HELLO WORLD", upperCase)
+
+    [<EntryPoint>]
+    let main _ = 0
