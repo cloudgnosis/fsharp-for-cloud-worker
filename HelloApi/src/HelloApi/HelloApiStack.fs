7c55a0439a222853dcb989b0de08f75e93c1ccbb
Erik Lundevall Zara <45368867+eriklz@users.noreply.github.com>
2020-09-28 10:28:30 +0200

    First CDK update

--- HelloApi/src/HelloApi/HelloApiStack.fs
+++ HelloApi/src/HelloApi/HelloApiStack.fs
@@ -1,8 +1,18 @@
 namespace HelloApi
 
+open System
 open Amazon.CDK
+open Amazon.CDK.AWS.Lambda
+open Amazon.CDK.AWS.Logs
 
 type HelloApiStack(scope, id, props) as this =
     inherit Stack(scope, id, props)
 
-    // The code that defines your stack goes here
+    let mutable funcProps = FunctionProps()
+    do funcProps.Runtime <- Runtime.DOTNET_CORE_3_1
+       funcProps.Code <- Code.FromAsset("./src/Backend/bin/Debug/netcoreapp3.1/publish")
+       funcProps.Handler <- "Backend::Backend.Function::FunctionHandler"
+       funcProps.Description <- "Our backend Lambda function"
+       funcProps.MemorySize <- Nullable<float>(256.0)
+       funcProps.LogRetention <- Option.toNullable(Some RetentionDays.ONE_WEEK)
+    let backend = Function(this, "Backend-function", funcProps)
