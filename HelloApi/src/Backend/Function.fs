e5b9d73df2aea28e3f18b3ead2836a00c6940a6f
Erik Lundevall Zara <45368867+eriklz@users.noreply.github.com>
2020-09-30 10:33:11 +0200

    Adding API Gateway

--- HelloApi/src/Backend/Function.fs
+++ HelloApi/src/Backend/Function.fs
@@ -2,8 +2,7 @@ namespace Backend
 
 
 open Amazon.Lambda.Core
-
-open System
+open Amazon.Lambda.APIGatewayEvents
 
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -12,13 +11,8 @@ open System
 
 
 type Function() =
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    member __.FunctionHandler (input: string) (_: ILambdaContext) =
-        match input with
-        | null -> String.Empty
-        | _ -> input.ToUpper()
+    member __.FunctionHandler (request: APIGatewayProxyRequest) (context: ILambdaContext) =
+        context.Logger.Log(sprintf "HttpMethod: %s, Path: %s" request.HttpMethod request.Path)
+        let response = APIGatewayProxyResponse(StatusCode = if request.HttpMethod = "GET" then 200 else 400)
+        context.Logger.Log(sprintf "Response statusCode %d" response.StatusCode)
+        response
--- HelloApi/src/HelloApi/HelloApiStack.fs
+++ HelloApi/src/HelloApi/HelloApiStack.fs
@@ -4,15 +4,26 @@ open System
 open Amazon.CDK
 open Amazon.CDK.AWS.Lambda
 open Amazon.CDK.AWS.Logs
+open Amazon.CDK.AWS.APIGateway
 
 type HelloApiStack(scope, id, props) as this =
     inherit Stack(scope, id, props)
 
-    let mutable funcProps = FunctionProps()
-    do funcProps.Runtime <- Runtime.DOTNET_CORE_3_1
-       funcProps.Code <- Code.FromAsset("./src/Backend/bin/Debug/netcoreapp3.1/publish")
-       funcProps.Handler <- "Backend::Backend.Function::FunctionHandler"
-       funcProps.Description <- "Our backend Lambda function"
-       funcProps.MemorySize <- Nullable<float>(256.0)
-       funcProps.LogRetention <- Option.toNullable(Some RetentionDays.ONE_WEEK)
-    let backend = Function(this, "Backend-function", funcProps)
+    let funcProps =
+        FunctionProps(Runtime = Runtime.DOTNET_CORE_3_1,
+                      Code = Code.FromAsset("./src/Backend/bin/Debug/netcoreapp3.1/publish"),
+                      Handler = "Backend::Backend.Function::FunctionHandler",
+                      Description = "Our backend Lambda function",
+                      MemorySize = Nullable<float>(256.0),
+                      LogRetention = Option.toNullable (Some RetentionDays.ONE_WEEK))
+
+    let backend =
+        Function(this, "Backend-function", funcProps)
+
+    let apiProps =
+        LambdaRestApiProps
+            (Description = "A simple example API backed by lambda using CDK",
+             EndpointExportName = "hello-api",
+             Handler = backend)
+
+    let gateway = LambdaRestApi(this, "api", apiProps)
